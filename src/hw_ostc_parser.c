cba786df2fbb68ae80aed87a4ce9418b8249c264
Anton Lundin <glance@acc.umu.se>
2016-08-19 23:21:39 +0200

    Fix deco model strings in hw parser
    The new version of the log book format broke the string handling.
    
    Signed-off-by: Anton Lundin <glance@acc.umu.se>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/hw_ostc_parser.c
+++ src/hw_ostc_parser.c
@@ -561,11 +561,11 @@ hw_ostc_parser_get_field (dc_parser_t *abstract, dc_field_type_t type, unsigned 
 				break;
 			case 4: /* Deco model */
 				string->desc = "Deco model";
-				if ((version == 0x23 && data[layout->decomode] == OSTC3_ZHL16) ||
+				if (((version == 0x23 || version == 0x24) && data[layout->decomode] == OSTC3_ZHL16) ||
 						(version == 0x22 && data[layout->decomode] == FROG_ZHL16) ||
 						(version == 0x21 && (data[layout->decomode] == OSTC_ZHL16_OC || data[layout->decomode] == OSTC_ZHL16_CC)))
 					strncpy(buf, "ZH-L16", BUFLEN);
-				if ((version == 0x23 && data[layout->decomode] == OSTC3_ZHL16_GF) ||
+				if (((version == 0x23 || version == 0x24) && data[layout->decomode] == OSTC3_ZHL16_GF) ||
 						(version == 0x22 && data[layout->decomode] == FROG_ZHL16_GF) ||
 						(version == 0x21 && (data[layout->decomode] == OSTC_ZHL16_OC_GF || data[layout->decomode] == OSTC_ZHL16_CC_GF)))
 					strncpy(buf, "ZH-L16-GF", BUFLEN);
@@ -574,11 +574,11 @@ hw_ostc_parser_get_field (dc_parser_t *abstract, dc_field_type_t type, unsigned 
 				break;
 			case 5: /* Deco model info */
 				string->desc = "Deco model info";
-				if ((version == 0x23 && data[layout->decomode] == OSTC3_ZHL16) ||
+				if (((version == 0x23 || version == 0x24) && data[layout->decomode] == OSTC3_ZHL16) ||
 						(version == 0x22 && data[layout->decomode] == FROG_ZHL16) ||
 						(version == 0x21 && (data[layout->decomode] == OSTC_ZHL16_OC || data[layout->decomode] == OSTC_ZHL16_CC)))
 					snprintf(buf, BUFLEN, "Saturation %u, Desaturation %u", layout->deco_info1, layout->deco_info2);
-				if ((version == 0x23 && data[layout->decomode] == OSTC3_ZHL16_GF) ||
+				if (((version == 0x23 || version == 0x24) && data[layout->decomode] == OSTC3_ZHL16_GF) ||
 						(version == 0x22 && data[layout->decomode] == FROG_ZHL16_GF) ||
 						(version == 0x21 && (data[layout->decomode] == OSTC_ZHL16_OC_GF || data[layout->decomode] == OSTC_ZHL16_CC_GF)))
 					snprintf(buf, BUFLEN, "GF %u/%u", data[layout->deco_info1], data[layout->deco_info2]);
