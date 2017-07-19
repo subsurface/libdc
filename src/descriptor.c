aa0b522ab86f06ef72dca30d1f9dc86c93f0b15c
Dirk Hohndel <dirk@hohndel.org>
2017-07-19 06:24:04 -0700

    Mark 3 more Shearwater dive computers as BLE
    Technically this is inaccurate because while some Perdix and Petrel 2
    models do in fact support dual stack, the Petrel only does BT. But
    the Petrel 2 identifies itself as Petrel via BT, so we need to flag
    them both here.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/descriptor.c
+++ src/descriptor.c
@@ -285,10 +285,10 @@ static const dc_descriptor_t g_descriptors[] = {
 	/* Shearwater Predator */
 	{"Shearwater", "Predator", DC_FAMILY_SHEARWATER_PREDATOR, 2},  // BT
 	/* Shearwater Petrel family */
-	{"Shearwater", "Petrel",    DC_FAMILY_SHEARWATER_PETREL, 3},  // BT
-	{"Shearwater", "Petrel 2",  DC_FAMILY_SHEARWATER_PETREL, 4},  // BT
+	{"Shearwater", "Petrel",    DC_FAMILY_SHEARWATER_PETREL, 3},  // BT // BLE
+	{"Shearwater", "Petrel 2",  DC_FAMILY_SHEARWATER_PETREL, 4},  // BT // BLE
 	{"Shearwater", "Nerd",      DC_FAMILY_SHEARWATER_PETREL, 5},  // BT
-	{"Shearwater", "Perdix",    DC_FAMILY_SHEARWATER_PETREL, 6},  // BT
+	{"Shearwater", "Perdix",    DC_FAMILY_SHEARWATER_PETREL, 6},  // BT // BLE
 	{"Shearwater", "Perdix AI", DC_FAMILY_SHEARWATER_PETREL, 7},  // BLE
 	/* Dive Rite NiTek Q */
 	{"Dive Rite", "NiTek Q",   DC_FAMILY_DIVERITE_NITEKQ, 0},
