3a2dc6cce4e308d8b1002e9a2b4a713102e4d85e
Dirk Hohndel <dirk@hohndel.org>
2023-03-29 11:24:43 -0700

    Adapt dctool to recent API change
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- examples/dctool_fwupdate.c
+++ examples/dctool_fwupdate.c
@@ -94,7 +94,7 @@ fwupdate (dc_context_t *context, dc_descriptor_t *descriptor, dc_transport_t tra
 		rc = hw_ostc_device_fwupdate (device, hexfile);
 		break;
 	case DC_FAMILY_HW_OSTC3:
-		rc = hw_ostc3_device_fwupdate (device, hexfile);
+		rc = hw_ostc3_device_fwupdate (device, hexfile, false);
 		break;
 	case DC_FAMILY_DIVESYSTEM_IDIVE:
 		rc = divesystem_idive_device_fwupdate (device, hexfile);
